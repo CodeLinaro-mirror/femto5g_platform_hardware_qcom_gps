f1950b745932294d7484b465f8ba64160e8c51e3
Mike Cailean <mcailean@codeaurora.org>
2018-02-06 16:36:27 -0800

    Support Multiband feature
    Add carrierFrequency to GNSS Measurements
    reporting. Also, add AGC level from QMI LOC
    message to GNSS measurements (as opposed to
    adding it from NMEA debug messages).
    
    CRs-fixed: 2186144
    
    Change-Id: Ia3995ba9e09ef4c68e8922b7570f7af0eb50098a

--- location/LocationDataTypes.h
+++ location/LocationDataTypes.h
@@ -358,9 +358,10 @@ typedef enum {
 
 typedef uint16_t GnssSvOptionsMask;
 typedef enum {
-    GNSS_SV_OPTIONS_HAS_EPHEMER_BIT = (1<<0),
-    GNSS_SV_OPTIONS_HAS_ALMANAC_BIT = (1<<1),
-    GNSS_SV_OPTIONS_USED_IN_FIX_BIT = (1<<2),
+    GNSS_SV_OPTIONS_HAS_EPHEMER_BIT             = (1<<0),
+    GNSS_SV_OPTIONS_HAS_ALMANAC_BIT             = (1<<1),
+    GNSS_SV_OPTIONS_USED_IN_FIX_BIT             = (1<<2),
+    GNSS_SV_OPTIONS_HAS_CARRIER_FREQUENCY_BIT   = (1<<3),
 } GnssSvOptionsBits;
 
 typedef enum {
@@ -467,6 +468,7 @@ typedef enum {
     GNSS_AIDING_DATA_SV_NO_EXIST_BIT     = (1<<8), // SV does not exist
     GNSS_AIDING_DATA_SV_IONOSPHERE_BIT   = (1<<9), // ionosphere correction
     GNSS_AIDING_DATA_SV_TIME_BIT         = (1<<10),// reset satellite time
+    GNSS_AIDING_DATA_SV_MB_DATA          = (1 << 11),// delete multiband data
 } GnssAidingDataSvBits;
 
 typedef uint32_t GnssAidingDataSvTypeMask;
@@ -889,6 +891,7 @@ typedef struct {
     float azimuth;     // azimuth of SV (in degrees)
     GnssSvOptionsMask gnssSvOptionsMask; // Bitwise OR of GnssSvOptionsBits
     GnssSignalTypeMask gnssSignalTypeMask; // Specifies GNSS signal type
+    float carrierFrequencyHz; // carrier frequency of the signal tracked
 } GnssSv;
 
 typedef struct {
