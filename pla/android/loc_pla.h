6747153b3a9e0d2b044756e29a7abb85eaf752d0
Kevin Tang <zhikait@codeaurora.org>
2020-01-10 12:17:36 -0800

    alias elapsedRealtime for le pla
    elapsedRealtime is under android namespace. To make it
    common for le and android, we alias elapsedRealtime so
    that android namespace comes with it.
    
    for LE build, we defined elapsedRealtime.
    
    Change-Id: Iaa1a2113d0783d31cb8a6f617b158ec264b9d955
    CRs-Fixed: 2598670

--- pla/android/loc_pla.h
+++ pla/android/loc_pla.h
@@ -28,7 +28,7 @@
 /*
 Changes from Qualcomm Innovation Center are provided under the following license:
 
-Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted (subject to the limitations in the
@@ -66,7 +66,8 @@ IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #ifdef __cplusplus
 #include <utils/SystemClock.h>
-#define uptimeMillis android::uptimeMillis
+#define uptimeMillis() android::uptimeMillis()
+#define elapsedRealtime() android::elapsedRealtime()
 #endif
 
 #ifdef __cplusplus
