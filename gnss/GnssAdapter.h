b0d855905bf70d40ca7822540f7fb50f3a713272
Wei (GPS SD) Chen <weic@codeaurora.org>
2021-07-20 14:39:12 -0700

    GPS HAL layer support for integrity risk and protection risk 1: API to set the integrity risk level for protection cal 2: output protection levels in PVT report
    Change-Id: I405b1c6abe083a589e36132562fde610c1626af8
    CRs-fixed: 2993619

--- gnss/GnssAdapter.h
+++ gnss/GnssAdapter.h
@@ -260,6 +260,7 @@ class GnssAdapter : public LocAdapterBase {
     std::queue<GnssLatencyInfo> mGnssLatencyInfoQueue;
     GnssReportLoggerUtil mLogger;
     bool mDreIntEnabled;
+    bool mPpeEnabled;
 
     /* === Misc callback from QMI LOC API ============================================== */
     GnssEnergyConsumedCallback mGnssEnergyConsumedCb;
@@ -441,6 +442,8 @@ public:
     uint32_t configEngineRunStateCommand(PositioningEngineMask engType,
                                          LocEngineRunState engState);
     uint32_t configOutputNmeaTypesCommand(GnssNmeaTypesMask enabledNmeaTypes);
+    uint32_t configEngineIntegrityRiskCommand(PositioningEngineMask engType,
+                                              uint32_t integrityRisk);
 
     /* ========= ODCPI ===================================================================== */
     /* ======== COMMANDS ====(Called from Client Thread)==================================== */
