bfddd978293dc364de797be7f4441d0cf68b0019
Harikrishnan Hariharan <hahariha@codeaurora.org>
2021-07-30 10:29:22 +0530

    Ni Notify Verify mask should be set when init GnssAdapter
    Currently the mask is set only when a location gnss client is
    running. The mask should be set in setConfig when GnssAdaper
    is initialized, so that we get Ni request even when there are
    no Gnss client apps running.
    
    Change-Id: Ib789cb16eaecfc98c57e794c426fe91224b0f22c
    CRs-Fixed: 2949185

--- gnss/GnssAdapter.cpp
+++ gnss/GnssAdapter.cpp
@@ -882,6 +882,7 @@ GnssAdapter::setConfigCommand()
             mApi(api) {}
         inline virtual void proc() const {
 
+            mAdapter.updateClientsEventMask();
             // set nmea mask type
             uint32_t mask = 0;
             if (NMEA_PROVIDER_MP == ContextBase::mGps_conf.NMEA_PROVIDER) {
@@ -2452,9 +2453,6 @@ GnssAdapter::updateClientsEventMask()
             it->second.engineLocationsInfoCb != nullptr) {
             mask |= LOC_API_ADAPTER_BIT_PARSED_POSITION_REPORT;
         }
-        if (it->second.gnssNiCb != nullptr) {
-            mask |= LOC_API_ADAPTER_BIT_NI_NOTIFY_VERIFY_REQUEST;
-        }
         if (it->second.gnssSvCb != nullptr) {
             mask |= LOC_API_ADAPTER_BIT_SATELLITE_REPORT;
         }
@@ -2504,6 +2502,9 @@ GnssAdapter::updateClientsEventMask()
     if (nullptr != mOdcpiRequestCb) {
         mask |= LOC_API_ADAPTER_BIT_REQUEST_WIFI;
     }
+
+    // always register for NI NOTIFY VERIFY to handle internally in HAL
+    mask |= LOC_API_ADAPTER_BIT_NI_NOTIFY_VERIFY_REQUEST;
 
     // need to register for leap second info
     // for proper nmea generation
