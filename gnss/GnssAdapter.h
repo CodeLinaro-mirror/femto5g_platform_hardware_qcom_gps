3e2b41bd6c710401c54d51e79435688cb55893e6
Wei (GPS SD) Chen <quic_weic@quicinc.com>
2021-12-17 11:09:25 -0800

    GPS HAL: clean up code that populates tech mask field in PVT report
    Use tech mask in GpsLocationExtended to populate the tech mask in
    Location struct of Location api
    
    Change-Id: I2c664946d3cfd0a81ebb5f7d9b63c7edba3600b8
    CRs-fixed: 3096764

--- gnss/GnssAdapter.h
+++ gnss/GnssAdapter.h
@@ -305,8 +305,7 @@ class GnssAdapter : public LocAdapterBase {
     /*==== CONVERSION ===================================================================*/
     static void convertOptions(LocPosMode& out, const TrackingOptions& trackingOptions);
     static void convertLocation(Location& out, const UlpLocation& ulpLocation,
-                                const GpsLocationExtended& locationExtended,
-                                const LocPosTechMask techMask);
+                                const GpsLocationExtended& locationExtended);
     static void convertLocationInfo(GnssLocationInfoNotification& out,
                                     const GpsLocationExtended& locationExtended,
                                     loc_sess_status status);
