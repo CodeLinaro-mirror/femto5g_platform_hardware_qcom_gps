448e50215d89c851c18f6f7ede1973600f812c2f
Hoss Zhou <quic_zhouh@quicinc.com>
2022-03-24 18:56:22 +0800

    revert LF copyright year and add QUIC copyright
    Change-Id: I363970aafec4098f9f0b6cb6f9085eabfa03d147
    CRs-Fixed: 3158479

--- utils/gps_extended_c.h
+++ utils/gps_extended_c.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2022 The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2021 The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -26,6 +26,8 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 /*
+Changes from Qualcomm Innovation Center are provided under the following license:
+
 Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -58,6 +60,7 @@ IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
 OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
+
 #ifndef GPS_EXTENDED_C_H
 #define GPS_EXTENDED_C_H
 
