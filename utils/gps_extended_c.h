50afd6a80ee867dc53a3378b778ed238303e267f
Yan Huang <quic_huang@quicinc.com>
2022-04-28 14:58:28 -0700

    Register with QMI_LOC_EVENT_MASK_FEATURE_STATUS_V02
    The QWES license status will be reported to HLOS
    in QMI_LOC_EVENT_REPORT_IND if this new mask is set.
    
    Change-Id: Idb1736ea11329a0f1d25306a01ca33df4e7ea384
    CRs-Fixed: 3171840

--- utils/gps_extended_c.h
+++ utils/gps_extended_c.h
@@ -184,7 +184,11 @@ typedef enum {
     /**<  Support the Multiple Attribution Apps(UTH clients Lock control) feature   */
     LOC_SUPPORTED_FEATURE_MULTIPLE_ATTRIBUTION_APPS,
     /**< Support the FLP, NLP Z-Source provider feature */
-    LOC_SUPPORTED_FEATURE_QMI_FLP_NLP_SOURCE
+    LOC_SUPPORTED_FEATURE_QMI_FLP_NLP_SOURCE,
+    /**< Support the feature to report engine debug data */
+    LOC_SUPPORTED_FEATURE_ENGINE_DEBUG_DATA,
+    /**< Support the feature to report feature update in QMI_LOC_EVENT_REPORT_IND */
+    LOC_SUPPORTED_FEATURE_DYNAMIC_FEATURE_STATUS
 } loc_supported_feature_enum;
 
 typedef struct {
@@ -1080,6 +1084,7 @@ enum loc_api_adapter_event_index {
     LOC_API_ADAPTER_EVENT_REPORT_INFO,                 // Event report info
     LOC_API_ADAPTER_LATENCY_INFORMATION_REPORT,        // Latency information report
     LOC_API_ADAPTER_REPORT_DISASTER_CRISIS,            // Disaster crisis message report
+    LOC_API_ADAPTER_FEATURE_STATUS_UPDATE,             // Dynamic feature status update
     LOC_API_ADAPTER_EVENT_MAX
 };
 
@@ -1125,6 +1130,8 @@ enum loc_api_adapter_event_index {
 #define LOC_API_ADAPTER_BIT_LATENCY_INFORMATION              (1ULL<<LOC_API_ADAPTER_LATENCY_INFORMATION_REPORT)
 #define LOC_API_ADAPTER_BIT_DISASTER_CRISIS_REPORT \
         (1ULL<<LOC_API_ADAPTER_REPORT_DISASTER_CRISIS)
+#define LOC_API_ADAPTER_BIT_FEATURE_STATUS_UPDATE  \
+        (1ULL<<LOC_API_ADAPTER_FEATURE_STATUS_UPDATE)
 
 typedef uint64_t LOC_API_ADAPTER_EVENT_MASK_T;
 
