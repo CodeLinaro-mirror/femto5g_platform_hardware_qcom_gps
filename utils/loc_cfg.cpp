722ca8f6f6ad3dee5ef2cd03477e5117c7de71d3
Wei (GPS SD) Chen <quic_weic@quicinc.com>
2022-09-07 10:49:21 -0700

    LIA API: add ability to config nmea datum type runtime
    LIA API: add support to runtime config nmea datum type
    
    CRs-fixed: 3272218
    
    Change-Id: I0f0561f5a5db11a7b8f6941bd69bab2d2057a026

--- utils/loc_cfg.cpp
+++ utils/loc_cfg.cpp
@@ -100,7 +100,6 @@ static const loc_param_s_type loc_param_table[] =
 {
     {"DEBUG_LEVEL",             &DEBUG_LEVEL,        NULL, 'n'},
     {"TIMESTAMP",               &TIMESTAMP,          NULL, 'n'},
-    {"DATUM_TYPE",              &DATUM_TYPE,         NULL, 'n'},
     {"LOG_BUFFER_ENABLED",      &sLogBufferEnabled,  NULL, 'n'},
 };
 static const int loc_param_num = sizeof(loc_param_table) / sizeof(loc_param_s_type);
@@ -126,29 +125,6 @@ const char LOC_PATH_QUIPC_CONF[] = LOC_PATH_QUIPC_CONF_STR;
 const char LOC_PATH_SLIM_CONF[] = LOC_PATH_SLIM_CONF_STR;
 const char LOC_PATH_VPE_CONF[] = LOC_PATH_VPE_CONF_STR;
 const char LOC_PATH_QPPE_CONF[] = LOC_PATH_QPPE_CONF_STR;
-
-/*===========================================================================
-FUNCTION loc_get_datum_type
-
-DESCRIPTION
-   get datum type
-
-PARAMETERS:
-   N/A
-
-DEPENDENCIES
-   N/A
-
-RETURN VALUE
-   DATUM TYPE
-
-SIDE EFFECTS
-   N/A
-===========================================================================*/
-int loc_get_datum_type()
-{
-    return DATUM_TYPE;
-}
 
 /*===========================================================================
 FUNCTION loc_set_config_entry
