c7c9b51b5bda4c06c750555af4493ce6568ddd06
Harikrishnan Hariharan <quic_hahariha@quicinc.com>
2022-11-09 19:42:19 +0530

    HAL: Remove usage of NO_UNORDERED_SET_OR_MAP
    This was added to support some legacy SIs with older version of GCC
    which did not have support for unordered_set/unordered_map. This
    will not be required in newer SIs which is guaranteed to have newer
    versions which support unordered_set/unordered_map.
    
    Change-Id: I2c4c0089fecdbcd07dfd3141abbb380df78f73a9
    CRs-Fixed: 3332702

--- core/EngineHubProxyBase.h
+++ core/EngineHubProxyBase.h
@@ -30,12 +30,7 @@
 
 #ifndef ENGINE_HUB_PROXY_BASE_H
 #define ENGINE_HUB_PROXY_BASE_H
-#ifdef NO_UNORDERED_SET_OR_MAP
-    #include <map>
-    #define unordered_map map
-#else
-    #include <unordered_map>
-#endif
+#include <unordered_map>
 
 namespace loc_core {
 
