shelby-stuffs/android_hardware_qcom_gps

GPS-HAL: Support Ephemeris reporting till LCA level
Adds Ephemeris structure in LocationDataTypes header,
implements wrapper function for passing data from HAL
to LHD

Change-Id: I2607e8d86b1bb97ebb70f4d773b384025658f9f6
CRs-Fixed: 3671164

location/LocationAPI.cpp
@@ -28,7 +28,7 @@
 /*
 Changes from Qualcomm Innovation Center are provided under the following license:
 
-Copyright (c) 2022, 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+Copyright (c) 2022, 2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted (subject to the limitations in the
@@ -199,7 +199,8 @@ static bool isGnssClient(LocationCallbacks& locationCallbacks)
             locationCallbacks.gnssNHzMeasurementsCb != nullptr ||
             locationCallbacks.locationSystemInfoCb != nullptr ||
             locationCallbacks.gnssDcReportCb != nullptr ||
-            locationCallbacks.gnssSignalTypesCb != nullptr);
+            locationCallbacks.gnssSignalTypesCb != nullptr ||
+            locationCallbacks.svEphemerisCb != nullptr);
 }
 
 static bool isBatchingClient(LocationCallbacks& locationCallbacks)

CodeLinaro-mirror/femto5g_platform_hardware_qcom_gps

location/LocationAPI.cpp
@@ -26,10 +26,9 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 /*
-Changes from Qualcomm Innovation Center are provided under the following license:
+Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
 
-Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
-
+Copyright (c) 2022, 2024 Qualcomm Innovation Center, Inc. All rights reserved.
 Redistribution and use in source and binary forms, with or without
 modification, are permitted (subject to the limitations in the
 disclaimer below) provided that the following conditions are met:
@@ -139,7 +138,8 @@ static bool isGnssClient(LocationCallbacks& locationCallbacks)
             locationCallbacks.gnssDataCb != nullptr ||
             locationCallbacks.gnssMeasurementsCb != nullptr ||
             locationCallbacks.gnssNHzMeasurementsCb != nullptr ||
-            locationCallbacks.locationSystemInfoCb != nullptr);
+            locationCallbacks.locationSystemInfoCb != nullptr ||
+            locationCallbacks.svEphemerisCb != nullptr);
 }
 
 static bool isFlpClient(LocationCallbacks& locationCallbacks)