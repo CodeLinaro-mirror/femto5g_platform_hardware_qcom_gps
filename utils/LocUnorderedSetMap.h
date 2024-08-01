d98f25c2bfe0c3547cb9cfbfee32e6d35c34d2e7
yingjiew <quic_yingjiew@quicinc.com>
2024-08-01 17:01:05 +0800

    Avoid access of deprecated LocUnorderedSetMap entry
    When there is no client listens to some DataItemId, we will remove
    the map entry. Can't access the deleted entry's iterator anymore.
    
    Change-Id: Id64aadebf7362667fbcf0fd5191f888ed9241727
    CRs-Fixed: 3886536

--- utils/LocUnorderedSetMap.h
+++ utils/LocUnorderedSetMap.h
@@ -26,6 +26,13 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  */
+/*
+Changes from Qualcomm Innovation Center are provided under the following license:
+
+Copyright (c) 2022, 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+SPDX-License-Identifier: BSD-3-Clause-Clear
+*/
+
 #ifndef __LOC_UNORDERDED_SETMAP_H__
 #define __LOC_UNORDERDED_SETMAP_H__
 
@@ -141,8 +148,11 @@ public:
                              unordered_set<KEY>* goneKeys, unordered_set<VAL>* goneVals) {
         for (auto key : keys) {
             auto iter = mMap.find(key);
-            if (iter != mMap.end() && trimOrRemove(iter, rVals, goneVals) && nullptr != goneKeys) {
-                goneKeys->insert(iter->first);
+            if (iter != mMap.end()) {
+                KEY goneKey = iter->first;
+                if (trimOrRemove(iter, rVals, goneVals) && nullptr != goneKeys) {
+                    goneKeys->insert(goneKey);
+                }
             }
         }
     }
