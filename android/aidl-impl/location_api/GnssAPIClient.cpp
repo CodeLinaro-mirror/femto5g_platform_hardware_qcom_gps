AOSPA/android_hardware_qcom_gps

Set sv pvt capability bit only when it is supported
Change-Id: I7af3435e07cb836fa2b012f7f40adf2ff5b184f1
CRs-Fixed: 3353834

android/aidl-impl/location_api/GnssAPIClient.cpp
@@ -444,7 +444,9 @@ void GnssAPIClient::updateCapabilities(LocationCapabilitiesMask capabilitiesMask
     if (capabilitiesMask & LOCATION_CAPABILITIES_ANTENNA_INFO) {
         data |= IGnssCallback::CAPABILITY_ANTENNA_INFO;
     }
-    data |= IGnssCallback::CAPABILITY_SATELLITE_PVT;
+    if (capabilitiesMask & LOCATION_CAPABILITIES_QWES_SV_POLYNOMIAL_BIT) {
+        data |= IGnssCallback::CAPABILITY_SATELLITE_PVT;
+    }
 
     IGnssCallback::GnssSystemInfo gnssInfo = { .yearOfHw = 2015, "aidl-impl" };
 

CodeLinaro-mirror/femto5g_platform_hardware_qcom_gps

(cherry picked from commit d6096817d0f93761613e6777a78849999efe5db9)
(cherry picked from commit 433164aa4adf28138d93791c06c930cf815ce4a7)

android/aidl-impl/location_api/GnssAPIClient.cpp
@@ -157,7 +157,9 @@ void GnssAPIClient::onCapabilitiesCb(LocationCapabilitiesMask capabilitiesMask) 
         capabilities |= IGnssCallback::CAPABILITY_SATELLITE_BLOCKLIST;
     }
     // CORRELATION_VECTOR not supported.
-    capabilities |= IGnssCallback::CAPABILITY_SATELLITE_PVT;
+    if (capabilitiesMask & LOCATION_CAPABILITIES_QWES_SV_POLYNOMIAL_BIT) {
+        capabilities |= IGnssCallback::CAPABILITY_SATELLITE_PVT;
+    }
     if (capabilitiesMask & LOCATION_CAPABILITIES_MEASUREMENTS_CORRECTION_BIT) {
         capabilities |= IGnssCallback::CAPABILITY_MEASUREMENT_CORRECTIONS_FOR_DRIVING;
     }