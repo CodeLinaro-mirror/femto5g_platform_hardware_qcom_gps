ecf0c69ff4effcb2d76a759f1b6b688aeb2b87ce
kparmar <quic_kparmar@quicinc.com>
2024-08-26 17:47:12 +0530

    GNSS_HAL: Use PVT based time association
    Use GPS time from PVT reports for elasped real time
    calculations. In GPS disabled mode, Meas reports
    will not contain GPS time, as a result time
    compuatation logics will not be correct as they
    depend on GPS time for calculations.
    
    Change-Id: I3141d2408f83b6ad33ebdfe0bfe1211a1c29efb3
    CRs-Fixed: 3913371

--- gnss/GnssAdapter.cpp
+++ gnss/GnssAdapter.cpp
@@ -30,7 +30,7 @@
 /*
 Changes from Qualcomm Innovation Center are provided under the following license:
 
-Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted (subject to the limitations in the
@@ -5463,8 +5463,6 @@ GnssAdapter::reportGnssMeasurementsEvent(const GnssMeasurements& gnssMeasurement
         }
 
         inline virtual void proc() const {
-            mAdapter.mPositionElapsedRealTimeCal.saveGpsTimeAndQtimerPairInMeasReport(
-                    mGnssMeasurements.gnssSvMeasurementSet);
             mAdapter.reportGnssMeasurementData(mGnssMeasurements.gnssMeasNotification);
             if ((false == mGnssMeasurements.gnssSvMeasurementSet.isNhz) &&
                     mAdapter.mDGnssNeedReport) {
