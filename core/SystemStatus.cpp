4466a75f1e022075e299f749df47ac543c4cab9e
Challa Koti Reddy <quic_challako@quicinc.com>
2024-09-11 08:55:57 +0530

    Static Analysis fixes
    Adds initialization for couple of variables
    
    Change-Id: Ie47ee539e752e31a2c083a85598a7a966d08ad1a
    CRs-Fixed: 3921153
    (cherry picked from commit 74c02b6b0ed3100795d82f8a3c80b41000550c8c)

--- core/SystemStatus.cpp
+++ core/SystemStatus.cpp
@@ -30,7 +30,7 @@
 /*
 Changes from Qualcomm Innovation Center are provided under the following license:
 
-Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted (subject to the limitations in the
@@ -156,7 +156,11 @@ void SystemStatusXoState::dump()
  SystemStatusRfAndParams
 ******************************************************************************/
 SystemStatusRfAndParams::SystemStatusRfAndParams(const GnssEngineDebugDataInfo& info) :
-    mJammedSignalsMask(info.jammedSignalsMask) {
+    mJammedSignalsMask(info.jammedSignalsMask),
+    mJammerGps(0),
+    mJammerGlo(0),
+    mJammerBds(0),
+    mJammerGal(0) {
 
     if (info.jammerInd.size() > 0) {
          mJammerGps = info.jammerInd[GNSS_LOC_SIGNAL_TYPE_GPS_L1CA];
