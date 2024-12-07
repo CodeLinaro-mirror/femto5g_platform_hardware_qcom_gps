be9a8f77346f1c30c3c7e23521909b0e684e715c
Yan Huang <quic_huang@quicinc.com>
2024-12-06 17:15:38 -0800

    Add updateMccMnc() in SystemStatus.h
    Add the pipeline inside loc-hal-daemon layer to
    broadcast the MccMnc string which is set by
    the LIA:updateNetworkInfo()
    
    Change-Id: Id7ee2802bd4d738a5a32d1eed2ea908434f50271
    CRs-Fixed: 3982735

--- core/SystemStatus.cpp
+++ core/SystemStatus.cpp
@@ -993,6 +993,13 @@ bool SystemStatus::setDefaultGnssEngineStates(void)
     return true;
 }
 
+bool SystemStatus::updateMccMnc(string& mccMncCountry) {
+    SystemStatusMccMnc s(mccMncCountry);
+    mSysStatusObsvr.notify({&s.mDataItem});
+
+    return true;
+}
+
 /******************************************************************************
 @brief      API to handle connection status update event from GnssRil
 
