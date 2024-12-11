41d45adbd135bfe215f4d822ae85846a69ab4b5b
kparmar <quic_kparmar@quicinc.com>
2024-12-11 11:40:07 +0530

    GPS_HAL: Address possible static analysis issues
    Change-Id: I285ba315f7ece549de631b81f4f4b7f4e2c99b80
    CRs-Fixed: 4000307
    (cherry picked from commit 274e47050f02ad5926eb763b56871101d1a2ca81)

--- core/LocApiBase.h
+++ core/LocApiBase.h
@@ -93,13 +93,17 @@ int decodeAddress(char *addr_string, int string_size,
 #define MAX_ADAPTERS          10
 #define MAX_FEATURE_LENGTH    100
 
-#define TO_ALL_ADAPTERS(adapters, call)                                \
-    for (int i = 0; i < MAX_ADAPTERS && NULL != (adapters)[i]; i++) {  \
-        call;                                                          \
+#define TO_ALL_ADAPTERS(adapters, call)                              \
+    if (NULL != adapters)  {                                         \
+        for (int i = 0; i < MAX_ADAPTERS && NULL != (adapters)[i]; i++) {  \
+            call;                                                          \
+        }                                                                  \
     }
 
 #define TO_1ST_HANDLING_ADAPTER(adapters, call)                              \
-    for (int i = 0; i <MAX_ADAPTERS && NULL != (adapters)[i] && !(call); i++);
+    if (NULL != adapters)  {                                         \
+        for (int i = 0; i <MAX_ADAPTERS && NULL != (adapters)[i] && !(call); i++); \
+    }
 
 class LocAdapterBase;
 struct LocSsrMsg;
