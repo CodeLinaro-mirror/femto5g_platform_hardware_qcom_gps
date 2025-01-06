6f7ab4d456f65d9f98d23ed0e1451cad624ea3d5
Chandrasekhar Mattaparthy <quic_cmattapa@quicinc.com>
2025-01-06 11:19:42 +0530

    gps: disable GPS functionality for Aspen
    Temporarily disabled GPS functionality for Aspen to
    unblock compilation issues.
    
    Change-Id: I9d8f09c142787f3282fe25839df4a0cd59132187

--- gps_vendor_board.mk
+++ gps_vendor_board.mk
@@ -3,6 +3,7 @@
 # added to this exclude list to prevent building
 LOC_BOARD_PLATFORM_EXCLUDE_LIST := anorak
 LOC_BOARD_PLATFORM_EXCLUDE_LIST += niobe
+LOC_BOARD_PLATFORM_EXCLUDE_LIST += vienna
 
 #lowi only exception list
 LOC_BOARD_PLATFORM_LOWI_ONLY_LIST := niobe
