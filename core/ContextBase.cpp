65e3164ddf7e5012b7e01feef4550e26c7d22a65
Wei (GPS SD) Chen <quic_weic@quicinc.com>
2025-02-20 14:15:06 -0800

    gps.conf: increase SUPL_VER to 2.0.4 by default
    Current default SUPL_VER of 1.0.0 is too old (support 2G and 3G),
    increase to 2.0.4 that supports NR
    
    CRs-fixed: 4064513
    
    Change-Id: I3c0cee20c142550b9fa8b3af673b35a639033f13

--- core/ContextBase.cpp
+++ core/ContextBase.cpp
@@ -174,7 +174,7 @@ void ContextBase::readConfig()
 #else
         mGps_conf.GPS_LOCK = GNSS_CONFIG_GPS_LOCK_MO_AND_NI;
 #endif
-        mGps_conf.SUPL_VER = 0x10000;
+        mGps_conf.SUPL_VER = 0x00020004;
         mGps_conf.SUPL_MODE = 0x1;
         mGps_conf.SUPL_ES = 0;
         mGps_conf.CP_MTLR_ES = 0;
