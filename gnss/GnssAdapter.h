d187cc9b305143be6f890e0684bf49f21fa5716b
Naresh Munagala <quic_nareshm@quicinc.com>
2025-03-05 11:29:50 +0530

    remove extra context switch for engine position
    engine position report is already called from main hal
    worker thead. No need to keep this again in to message queue
    as it will delay the fused reporting.
    
    Change-Id: I33ea3b7e6b5658fe130e192b86a41a59c026aac1

--- gnss/GnssAdapter.h
+++ gnss/GnssAdapter.h
@@ -30,7 +30,7 @@
 /*
 Changes from Qualcomm Innovation Center are provided under the following license:
 
-Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted (subject to the limitations in the
@@ -679,8 +679,6 @@ public:
                                      LocPosTechMask techMask,
                                      GnssDataNotification* pDataNotify = nullptr,
                                      int msInWeek = -1);
-    void reportEnginePositionsEvent(unsigned int count,
-                                    EngineLocationInfo* locationArr);
 
     virtual void reportSvEvent(const GnssSvNotification& svNotify);
     virtual void reportNmeaEvent(const char* nmea, size_t length);
