e773da49718e2e72b0509ad0c65ae42e0657204a
Karan Singh Parmar <quic_kparmar@quicinc.com>
2025-05-07 19:43:09 +0530

    GNSS-HAL: Do not register for BANDS_SUPPORTED event
    Explicit registeration not needed as LocHALD bootup
    will automatically register for the event
    
    Change-Id: I2c12254babb4f61c0f5abc51a81491beb2ef53dc
    CRs-Fixed: 4134344

--- gnss/GnssAdapter.cpp
+++ gnss/GnssAdapter.cpp
@@ -3107,10 +3107,6 @@ GnssAdapter::updateClientsEventMask()
             LOC_API_ADAPTER_BIT_EVENT_REPORT_INFO |
             LOC_API_ADAPTER_BIT_FEATURE_STATUS_UPDATE;
 
-#ifdef FEATURE_AUTOMOTIVE
-    // Subscribe to get GNSS BAND supported information on bootup
-    mask |= LOC_API_ADAPTER_BIT_GNSS_BANDS_SUPPORTED;
-#endif
     for (auto it=mClientData.begin(); it != mClientData.end(); ++it) {
         if (it->second.trackingCb != nullptr ||
             it->second.gnssLocationInfoCb != nullptr ||
