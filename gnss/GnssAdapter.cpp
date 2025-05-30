eb9c2e3f593ed1365e706900258c266732461d1d
yingjiew <quic_yingjiew@quicinc.com>
2025-05-30 13:09:32 +0800

    Force MO session to standalone during E-911
    SM couldn't start xtra download for MSB supl mode, we need set as
    standalone to trigger xtra download during emergency.
    
    CRs-Fixed: 4167416
    Change-Id: I0af86314837b905f005c8e7fe91edbce3eadac07

--- gnss/GnssAdapter.cpp
+++ gnss/GnssAdapter.cpp
@@ -104,7 +104,6 @@ IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define GPS_LOCATION_DESIRED_FLAGS (LOC_GPS_LOCATION_HAS_LAT_LONG | LOC_GPS_LOCATION_HAS_ACCURACY)
 
 using namespace loc_core;
-
 static int loadEngHubForExternalEngine = 0;
 static int loadLocSlatePUNCModel = 0;
 static loc_param_s_type izatConfParamTable[] = {
@@ -187,6 +186,7 @@ GnssAdapter::GnssAdapter() :
     mCdfwInterface(nullptr),
     mDGnssNeedReport(false),
     mDGnssDataUsage(false),
+    mInEmergency(false),
     mOdcpiStateMask(0),
     mCallbackPriority(OdcpiPrioritytype::ODCPI_HANDLER_PRIORITY_LOW),
     mOdcpiTimer(this),
@@ -3405,8 +3405,6 @@ GnssAdapter::startTrackingCommand(LocationAPI* client, const TrackingOptions& op
                 if (mOptions.minInterval < minIntervalToSet) {
                     mOptions.minInterval = minIntervalToSet;
                 }
-                LOC_LOGd("Updated min Interval: %u, nHzEnabled: %s",
-                        mOptions.minInterval, nHzStatus ? "true" : "false");
 
                 if (GNSS_POWER_MODE_M4 == mOptions.powerMode &&
                         mOptions.tbm > TRACKING_TBM_THRESHOLD_MILLIS) {
@@ -3415,8 +3413,11 @@ GnssAdapter::startTrackingCommand(LocationAPI* client, const TrackingOptions& op
                     mOptions.powerMode = GNSS_POWER_MODE_M2;
                 }
 
-                // Update request SUPL mode if not specified
-                if (mOptions.mode == GNSS_SUPL_MODE_UNKNOWN) {
+                // In emergency: force request SUPL mode as standalone
+                // Non emergency: Update request SUPL mode if not specified
+                if (mAdapter.mInEmergency) {
+                    mOptions.mode = GNSS_SUPL_MODE_STANDALONE;
+                } else if (mOptions.mode == GNSS_SUPL_MODE_UNKNOWN) {
                     if (mAdapter.isAssistedGpsEnabled() &&
                             (ContextBase::mGps_conf.SUPL_MODE & GNSS_SUPL_MODE_MSB) != 0) {
                         mOptions.mode = GNSS_SUPL_MODE_MSB;
@@ -3425,6 +3426,11 @@ GnssAdapter::startTrackingCommand(LocationAPI* client, const TrackingOptions& op
                     }
                     LOC_LOGd("Updated UNKNOWN SUPL mode to %d", mOptions.mode);
                 }
+                LOC_LOGd("Updated min Interval: %u, nHzEnabled: %s, emergency: %d mode: %u, "
+                        "agps : %d, SUPL_MODE: %d",
+                        mOptions.minInterval, nHzStatus ? "true" : "false", mAdapter.mInEmergency,
+                        mOptions.mode, mAdapter.isAssistedGpsEnabled(),
+                        ContextBase::mGps_conf.SUPL_MODE);
 
                 // Api doesn't support multiple clients for time based tracking, so mutiplex
                 bool reportToClientWithNoWait =
@@ -5614,6 +5620,7 @@ void GnssAdapter::requestOdcpi(const OdcpiRequestInfo& request)
         // so the mOdcpiTimer helps avoid spamming the framework as well as
         // extending the odcpi session past 30 seconds if needed
         if (ODCPI_REQUEST_TYPE_START == request.type) {
+            mInEmergency = request.isEmergencyMode;
             if (!(mOdcpiStateMask & ODCPI_REQ_ACTIVE)  && false == mOdcpiTimer.isActive()) {
                 fireOdcpiRequest(request);
                 mOdcpiStateMask |= ODCPI_REQ_ACTIVE;
@@ -6048,6 +6055,7 @@ void GnssAdapter::odcpiTimerExpire()
         fireOdcpiRequest(mOdcpiRequest);
         mOdcpiTimer.restart();
     } else {
+        mInEmergency = false;
         mOdcpiTimer.stop();
     }
 }
