5f650a0ab04e407ebc62d1292e91d1c2503b21fa
Yingjie Wang <yingjiew@qti.qualcomm.com>
2025-06-24 14:28:37 +0800

    Use year-less copyright for opensource project
    Current OSS marking is long and needs update year after year.
    Replace with year-less copyright like proprietary project does
    
    Change-Id: I1fcb6bd08639043c7fd6cf7d347bab53c6dbf2d5
    CRs-Fixed: 4192473

--- android/aidl-impl/GnssVisibilityControl.cpp
+++ android/aidl-impl/GnssVisibilityControl.cpp
@@ -1,6 +1,5 @@
 /*
-Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
-Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 SPDX-License-Identifier: BSD-3-Clause-Clear
 */
 #include <aidl/android/hardware/gnss/visibility_control/BnGnssVisibilityControl.h>
