6d5ff3b08f038c5439185d81aaa5b348ed0a145a
Karan Singh Parmar <kparmar@qti.qualcomm.com>
2025-07-07 17:27:47 +0530

    GPS_HAL: Optimize CPU usage for auto start client
    1. Generate NMEA string only if client requested it.
    For default AUTO start client, it does not need NMEA, it only
    susbribe for PVT, SV info. For SV info, HLOS was blindly trying
    to generate NMEA string, this can cause unwanted CPU hogging.
    
    2. For AUTO, subscribe to ENGINE_DEBUG_DATA only if client
    request for GnssData or GnssMeasurement Information.
    
    Change-Id: I19ea018875c4485f59c29b416f6cea32be9102d3
    CRs-Fixed: 4179175

--- gnss/GnssAdapter.cpp
+++ gnss/GnssAdapter.cpp
@@ -61,6 +61,11 @@ IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
 OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
+/*
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
 
 #define LOG_NDEBUG 0
 #define LOG_TAG "LocSvc_GnssAdapter"
@@ -1043,7 +1048,9 @@ GnssAdapter::setConfigCommand()
                 // Modem does not provide noraml NMEA if ENGINE_DEBUG_DATA feature is available
                 // ensuring AP to nmea generation in this case
                 ContextBase::mGps_conf.NMEA_PROVIDER =  NMEA_PROVIDER_AP;
+#ifndef FEATURE_AUTOMOTIVE
                 mAdapter.updateEvtMask(LOC_API_ADAPTER_BIT_ENGINE_DEBUG_DATA_REPORT, LOC_REGISTRATION_MASK_ENABLED);
+#endif
             }
             std::string oldServerUrl = mAdapter.getServerUrl();
             std::string oldMoServerUrl = mAdapter.getMoServerUrl();
@@ -1096,7 +1103,9 @@ GnssAdapter::setConfigCommand()
                 // set nmea mask type
                 uint32_t mask = 0;
                 if (ContextBase::isFeatureSupported(LOC_SUPPORTED_FEATURE_ENGINE_DEBUG_DATA)) {
+#ifndef FEATURE_AUTOMOTIVE
                     mask |= LOC_API_ADAPTER_BIT_ENGINE_DEBUG_DATA_REPORT;
+#endif
                 } else {
                     if (NMEA_PROVIDER_MP == gpsConf.NMEA_PROVIDER) {
                         mask |= LOC_NMEA_ALL_GENERAL_SUPPORTED_MASK;
@@ -2640,7 +2649,9 @@ GnssAdapter::updateClientsEventMask()
             mask |= LOC_API_ADAPTER_BIT_PARSED_POSITION_REPORT;
         }
         if (ContextBase::isFeatureSupported(LOC_SUPPORTED_FEATURE_ENGINE_DEBUG_DATA)) {
+#ifndef FEATURE_AUTOMOTIVE
             mask |= LOC_API_ADAPTER_BIT_ENGINE_DEBUG_DATA_REPORT;
+#endif
         } else {
             if (it->second.gnssNmeaCb != nullptr) {
                 mLocApi->setNMEATypesSync(mNmeaMask);
@@ -2651,9 +2662,11 @@ GnssAdapter::updateClientsEventMask()
         }
         if (it->second.gnssMeasurementsCb != nullptr) {
             mask |= LOC_API_ADAPTER_BIT_GNSS_MEASUREMENT;
+            mask |= LOC_API_ADAPTER_BIT_ENGINE_DEBUG_DATA_REPORT;
         }
         if (it->second.gnssNHzMeasurementsCb != nullptr) {
             mask |= LOC_API_ADAPTER_BIT_GNSS_NHZ_MEASUREMENT;
+            mask |= LOC_API_ADAPTER_BIT_ENGINE_DEBUG_DATA_REPORT;
         }
         if (it->second.gnssDataCb != nullptr) {
             mask |= LOC_API_ADAPTER_BIT_PARSED_POSITION_REPORT;
@@ -4345,6 +4358,7 @@ GnssAdapter::reportSv(GnssSvNotification& svNotify)
     int numSv = svNotify.count;
     uint16_t gnssSvId = 0;
     uint64_t svUsedIdMask = 0;
+    bool needToReportNmea = false;
 
     for (int i=0; i < numSv; i++) {
         svUsedIdMask = 0;
@@ -4483,9 +4497,12 @@ GnssAdapter::reportSv(GnssSvNotification& svNotify)
         if (nullptr != it->second.gnssSvCb) {
             it->second.gnssSvCb(svNotify);
         }
+        if (!needToReportNmea && (nullptr != it->second.gnssNmeaCb)) {
+            needToReportNmea = true;
+        }
     }
 
-    if (NMEA_PROVIDER_AP == ContextBase::mGps_conf.NMEA_PROVIDER && !mTrackingSessions.empty()) {
+    if (needToReportNmea && (NMEA_PROVIDER_AP == ContextBase::mGps_conf.NMEA_PROVIDER) && (!mTrackingSessions.empty())) {
         std::vector<std::string> nmeaArraystr;
         loc_nmea_generate_sv(svNotify, nmeaArraystr);
         stringstream ss;
