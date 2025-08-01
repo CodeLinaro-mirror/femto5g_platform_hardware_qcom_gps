83b164c21264a28d92711fdc58a1fe52879d82ef
Wei (GPS SD) Chen <quic_weic@quicinc.com>
2025-08-01 15:34:44 -0700

    loc_log: remove unused routines
    1: remove log_get_name, loc_get_target_name and  loc_get_name
    2: define struct of ValStrPair to be used for array based log
       string table
    
    CRs-fixed: 4239875
    
    Change-Id: If7777702cdec5692cbb592f84a70d741c4deb942

--- utils/loc_log.h
+++ utils/loc_log.h
@@ -53,30 +53,27 @@ typedef unordered_map<int64_t, string> NameValTbl;
 #define NAME_VAL(x) {x, "" #x ""}
 #define DECLARE_TBL(T) static const NameValTbl T##_tbl
 
+struct ValStrPair {
+   // only good for tables with continuous entries and value with 0
+   int32_t     val;
+   const char* str;
+};
+
 extern const string gEmptyStr;
 extern const string gUnknownStr;
-
-#define CHECK_MASK(type, value, mask_var, mask) \
-   (((mask_var) & (mask)) ? (type) (value) : (type) (-1))
 
 #define LOC_TABLE_SIZE(table) (sizeof(table)/sizeof((table)[0]))
 
 #define FIELDVAL_DEC(field) \
         loc_put_tag_val(#field, to_string(field))
-#define FIELDVAL_DEC_ARR(field) \
-        loc_put_tag_val(#field, \
-                        loc_parenthesize(loc_prim_arr_to_string(field, \
-                                                                sizeof(field)/sizeof(field[0]))))
+
 #define FIELDVAL_HEX(field) \
         loc_put_tag_val(#field, to_string_hex(field))
-#define FIELDVAL_HEX_ARR(field) \
-        loc_put_tag_val(#field, \
-                        loc_parenthesize(loc_prim_arr_to_string(field, \
-                                                                sizeof(field)/sizeof(field[0]), \
-                                                                false)))
+
 #define FIELDVAL_ENUM(field, tbl) \
         loc_put_tag_val(#field, \
                         loc_get_name_from_tbl(tbl, field, gUnknownStr))
+
 #define FIELDVAL_MASK(field, tbl) \
         loc_put_tag_val(#field, \
                         to_string_hex((uint64_t)field) + " " + \
@@ -99,10 +96,6 @@ inline const char* loc_get_name_from_val(const NameValTbl& table, int64_t value)
     return loc_get_name_from_tbl(table, value, gUnknownStr).c_str();
 }
 
-inline const char* log_succ_fail_string(int is_succ) {
-    return is_succ? "successful" : "failed";
-}
-
 /* prints mask into a string with bit definitions from tbl */
 /* mask - bit mask, to be expanded into " BIT_NAMEx | BIT_NAMEy ... "
    tbl - a table with defs for each bit, defined as <bit, name> entries
@@ -111,7 +104,5 @@ inline const char* log_succ_fail_string(int is_succ) {
  */
 string loc_get_bit_defs(uint64_t mask, const NameValTbl& tbl);
 uint64_t loc_get_least_bit(uint64_t& mask, bool clearThebit = true);
-const char* loc_get_target_name(unsigned int target);
-char *loc_get_time(char *time_string, size_t buf_size);
 
 #endif /* LOC_LOG_H */
