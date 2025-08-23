AOSPA/android_hardware_qcom_gps

HAL: Support below Android U features in LE
Make sure elapsedRealtimeNanos is always valid
Add support for signalTypeCapabilities
Add support for isFullTracking

Change-Id: Ia2a3ec5516fd071e2f094cd1c7a831b48ce9a511
CRs-fixed: 3327362

location/LocationAPI.cpp
@@ -198,7 +198,8 @@ static bool isGnssClient(LocationCallbacks& locationCallbacks)
             locationCallbacks.gnssMeasurementsCb != nullptr ||
             locationCallbacks.gnssNHzMeasurementsCb != nullptr ||
             locationCallbacks.locationSystemInfoCb != nullptr ||
-            locationCallbacks.gnssDcReportCb != nullptr);
+            locationCallbacks.gnssDcReportCb != nullptr ||
+            locationCallbacks.gnssSignalTypesCb != nullptr);
 }
 
 static bool isBatchingClient(LocationCallbacks& locationCallbacks)

CodeLinaro-mirror/femto5g_platform_hardware_qcom_gps

location/LocationAPI.cpp
@@ -139,7 +139,8 @@ static bool isGnssClient(LocationCallbacks& locationCallbacks)
             locationCallbacks.gnssMeasurementsCb != nullptr ||
             locationCallbacks.gnssNHzMeasurementsCb != nullptr ||
             locationCallbacks.locationSystemInfoCb != nullptr ||
-            locationCallbacks.svEphemerisCb != nullptr);
+            locationCallbacks.svEphemerisCb != nullptr ||
+            locationCallbacks.gnssSignalTypesCb != nullptr);
 }
 
 static bool isFlpClient(LocationCallbacks& locationCallbacks)