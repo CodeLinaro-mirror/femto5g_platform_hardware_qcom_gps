d8d95c72278ea77f3549abe2fdee2cca5adce69a
Wei (GPS SD) Chen <quic_weic@quicinc.com>
2025-09-20 07:42:01 -0700

    Gnss adapter: PVT report to timestamp with current boot timestamp
    Use current boottimestamp to timestamp PVT report in below scenario:
    1: if bootimestamp can not be calculated for PVT report
    2: if bootimestamp is of future
    
    CRs-fixed: 4286581
    Change-Id: I18ab87e6db111221d8d35054cd8a1c88b50be86a
    (cherry picked from commit d7fa71c301b069c75a70dc16dd4a5321c41f74ef)

--- gnss/GnssAdapter.cpp
+++ gnss/GnssAdapter.cpp
@@ -447,29 +447,22 @@ void GnssAdapter::fillElapsedRealTime(const GpsLocationExtended& locationExtende
             out.elapsedRealTime = elapsedTimeNs;
             out.elapsedRealTimeUnc = (int64_t) (elapsedTimeUncMsec * 1000000);
         }
-#ifndef FEATURE_AUTOMOTIVE
-        else if ((out.timestamp > 0) &&
-                 (locationExtended.gpsTime.gpsWeek != UNKNOWN_GPS_WEEK_NUM)) {
-            int64_t locationTimeNanos = (int64_t)out.timestamp * 1000000;
-            bool isCurDataTimeTrustable = (out.timestamp % mLocPositionMode.min_interval == 0);
-            int64_t elapsedRealTime = mPositionElapsedRealTimeCal.getElapsedRealtimeEstimateNanos(
-                    locationTimeNanos, isCurDataTimeTrustable,
-                    (int64_t)mLocPositionMode.min_interval * 1000000);
-
-            if (elapsedRealTime != -1) {
-                out.flags |= LOCATION_HAS_ELAPSED_REAL_TIME_BIT;
-                out.elapsedRealTime = elapsedRealTime;
-                out.elapsedRealTimeUnc = mPositionElapsedRealTimeCal.getElapsedRealtimeUncNanos();
-            }
-        }
-#endif //FEATURE_AUTOMOTIVE
     }
-
 #ifndef FEATURE_AUTOMOTIVE
+    bool needToUseCurrentBootTime = false;
+    uint64_t currentBootTimeNs = getBootTimeMilliSec() * 1000000;
     if (!(out.flags & LOCATION_HAS_ELAPSED_REAL_TIME_BIT)) {
-        out.elapsedRealTime = getBootTimeMilliSec() * 1000000;
-        out.elapsedRealTimeUnc = mPositionElapsedRealTimeCal.getElapsedRealtimeUncNanos();
-        out.flags |= LOCATION_HAS_ELAPSED_REAL_TIME_BIT;
+       needToUseCurrentBootTime = true;
+       LOC_LOGw("can not calculate elapsed real time, set to current time");
+    } else if (out.elapsedRealTime > currentBootTimeNs) {
+       needToUseCurrentBootTime = true;
+       LOC_LOGw("elapsed real time is %" PRIu64 " nsec in future, set to current time",
+                out.elapsedRealTime - currentBootTimeNs);
+    }
+    if (needToUseCurrentBootTime) {
+       out.elapsedRealTime = currentBootTimeNs;
+       out.elapsedRealTimeUnc = mPositionElapsedRealTimeCal.getElapsedRealtimeUncNanos();
+       out.flags |= LOCATION_HAS_ELAPSED_REAL_TIME_BIT;
     }
 #endif //FEATURE_AUTOMOTIVE
 }
