0f7ddaa0a8f4e1cf6c3fe9ff8455bdaf69583ef2
Mobashshirur Rahman <mobashsh@qti.qualcomm.com>
2026-02-20 15:33:07 +0530

    PQW7 debug nmea parser: fix sv cnt mis-match issue
    When HLOS defined SV cnt mismatched with MP cnt, PQW7 will not be
    able to parse the debug nmea, rework the routine to remove this
    limitation
    
    CRs-Fixed: 4106244
    Change-Id: Ib20c39854da25df81df98c59ee898e223fd35c29

--- core/SystemStatus.cpp
+++ core/SystemStatus.cpp
@@ -801,31 +801,18 @@ SystemStatusPQWP7::SystemStatusPQWP7(const GnssEngineDebugDataInfo& gnssEngineDe
 class SystemStatusPQWP7parser : public SystemStatusNmeaBase
 {
 private:
-    enum
-    {
-        eTalker = 0,
-        eUtcTime = 1,
-        eMin = 2 + SV_ALL_NUM_MIN*3,
-        eMax = 2 + SV_ALL_NUM*3
-    };
     SystemStatusPQWP7 mP7;
 
+    // PQWP7 in format of {eTalker, eUtcTime, array of {type, source, agesec}}
 public:
     SystemStatusPQWP7parser(const char *str_in, uint32_t len_in)
         : SystemStatusNmeaBase(str_in, len_in)
     {
-        uint32_t svLimit = SV_ALL_NUM;
-        if (mField.size() < eMin) {
-            LOC_LOGE("PQWP7parser - invalid size=%zu", mField.size());
-            return;
-        }
-        if (mField.size() < eMax) {
-            // Try reducing limit, accounting for possibly missing NAVIC support
-            svLimit = SV_ALL_NUM_MIN;
-        }
+
+        uint32_t fieldSize = mField.size();
 
         memset(mP7.mNav, 0, sizeof(mP7.mNav));
-        for (uint32_t i=0; i<svLimit; i++) {
+        for (uint32_t i=0; (i*3+4) < fieldSize; i++) {
             mP7.mNav[i].mType   = GnssEphemerisType(atoi(mField[i*3+2].c_str()));
             mP7.mNav[i].mSource = GnssEphemerisSource(atoi(mField[i*3+3].c_str()));
             mP7.mNav[i].mAgeSec = atoi(mField[i*3+4].c_str());
