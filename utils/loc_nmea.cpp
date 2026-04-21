b4259904309ac1545f82a386e525463374923ad8
Karan Singh Parmar <kparmar@qti.qualcomm.com>
2026-04-21 14:26:48 +0530

    LOC_NMEA: Consider altitude Assumed for 2D/3D fix determination
    Consider using value of altitude assumed in position report for
    computing fix type in GSA NMEA sentence
    
    Change-Id: I6503666a59b5be5f677594af28df412a6994eaee
    CRs-Fixed: 4511143

--- utils/loc_nmea.cpp
+++ utils/loc_nmea.cpp
@@ -28,39 +28,9 @@
  */
 
 /*
-Changes from Qualcomm Innovation Center are provided under the following license:
-
-Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted (subject to the limitations in the
-disclaimer below) provided that the following conditions are met:
-
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-
-    * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-
-NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
-GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
-HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
-WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
-IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+Changes from Qualcomm Technologies, Inc. are provided under the following license:
+Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+SPDX-License-Identifier: BSD-3-Clause-Clear
 */
 
 #define LOG_NDEBUG 0
@@ -736,12 +706,15 @@ static uint32_t loc_nmea_generate_GSA(const UlpLocation &location,
             pMarker += lengthTagBlock;
             lengthRemaining -= lengthTagBlock;
         }
-        if (location.gpsLocation.flags & LOC_GPS_LOCATION_HAS_ALTITUDE) {
-            fixType = '3'; // 3D fix
-        } else if (location.gpsLocation.flags & LOC_GPS_LOCATION_HAS_LAT_LONG) {
-            fixType = '2'; // 2D fix
+        if (location.gpsLocation.flags & LOC_GPS_LOCATION_HAS_LAT_LONG) {
+            if ((location.gpsLocation.flags & LOC_GPS_LOCATION_HAS_ALTITUDE) &&
+                    (!locationExtended.altitudeAssumed)) {
+                fixType = '3'; // 3D fix
+            } else {
+                fixType = '2'; // 2D fix
+            }
         } else {
-            fixType = '1'; // no fix
+            fixType = '1'; //no fix
         }
 
         // Start printing the sentence
